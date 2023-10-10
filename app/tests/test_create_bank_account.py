629898c0272f1a988aec02046142091b8b09ce2e
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2023-10-10 14:47:04 +0200

    added pesel to Konto class, feature02 completed

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -6,13 +6,13 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
     name = "Dariusz"
     last_name = "Januszewski"
-
+    pesel = "12345678901"
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto(self.name, self.last_name)
+        pierwsze_konto = Konto(self.name, self.last_name,self.pesel)
         self.assertEqual(pierwsze_konto.imie, self.name, "Imie nie zostało zapisane!")
         self.assertEqual(
             pierwsze_konto.nazwisko, self.last_name, "Nazwisko nie zostało zapisane!"
         )
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-
+        self.assertEqual(pierwsze_konto.pesel, self.pesel, "Pesel nie został zapisany!")
     # tutaj proszę dodawać nowe testy
