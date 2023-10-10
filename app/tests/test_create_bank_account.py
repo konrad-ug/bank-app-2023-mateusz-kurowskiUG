6b5b7930a2709a5edd0c2359ca6a733e5fc2fc42
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2023-10-10 14:43:48 +0200

    object Konto added, feature01 completed

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -2,12 +2,17 @@ import unittest
 
 from ..Konto import Konto
 
+
 class TestCreateBankAccount(unittest.TestCase):
+    name = "Dariusz"
+    last_name = "Januszewski"
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        pierwsze_konto = Konto(self.name, self.last_name)
+        self.assertEqual(pierwsze_konto.imie, self.name, "Imie nie zostało zapisane!")
+        self.assertEqual(
+            pierwsze_konto.nazwisko, self.last_name, "Nazwisko nie zostało zapisane!"
+        )
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
 
-    #tutaj proszę dodawać nowe testy
+    # tutaj proszę dodawać nowe testy
