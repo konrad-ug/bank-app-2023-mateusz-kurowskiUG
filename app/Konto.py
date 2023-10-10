c79258ea79229502ed187d6301ad661d983d9f67
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2023-10-10 14:58:45 +0200

    test cases and logic for too long or too short pesels completed, feature03 done

--- app/Konto.py
+++ app/Konto.py
@@ -1,6 +1,9 @@
 class Konto:
-    def __init__(self, imie, nazwisko,pesel):
+    def __init__(self, imie, nazwisko, pesel):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
-        self.pesel = pesel
+        if len(pesel) != 11:
+            self.pesel = "Niepoprawny pesel!"
+        else:
+            self.pesel = pesel
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -7,12 +7,26 @@ class TestCreateBankAccount(unittest.TestCase):
     name = "Dariusz"
     last_name = "Januszewski"
     pesel = "12345678901"
+
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto(self.name, self.last_name,self.pesel)
+        pierwsze_konto = Konto(self.name, self.last_name, self.pesel)
         self.assertEqual(pierwsze_konto.imie, self.name, "Imie nie zostało zapisane!")
         self.assertEqual(
             pierwsze_konto.nazwisko, self.last_name, "Nazwisko nie zostało zapisane!"
         )
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
         self.assertEqual(pierwsze_konto.pesel, self.pesel, "Pesel nie został zapisany!")
-    # tutaj proszę dodawać nowe testy
+        self.assertEqual(pierwsze_konto.pesel, self.pesel, "Pesel nie został zapisany!")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Pesel nie ma 11 znaków!")
+
+    def test_tworzenie_konta_z_krotkim_pesel(self):
+        konto = Konto(self.name, self.last_name, "1234")
+        self.assertEqual(
+            konto.pesel, "Niepoprawny pesel!", "Pesel nie został zapisany!"
+        )
+
+    def test_tworzenie_konta_z_dlugim_pesel(self):
+        konto = Konto(self.name, self.last_name, "123456789123456789123456789")
+        self.assertEqual(
+            konto.pesel, "Niepoprawny pesel!", "Pesel nie został zapisany!"
+        )
