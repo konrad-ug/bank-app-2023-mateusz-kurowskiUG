ac45b30770411a79ae8c839c681308d2d4155ee6
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2023-10-17 15:59:31 +0200

    feature09 completed, but the code needs refactor

--- app/Account.py
+++ app/Account.py
@@ -14,5 +14,5 @@ class Account:
             self.saldo += amount
 
     def express_outgoing_transfer(self, amount):
-        if self.saldo >= amount + self.express_transfer_fee:
+        if self.saldo - 1 >= amount + self.express_transfer_fee:
             self.saldo -= amount + self.express_transfer_fee
