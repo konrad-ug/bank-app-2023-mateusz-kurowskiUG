fd988fe2194cfb8e8e004de36a317ed08115d130
Mateusz Kurowski <mkurowski@INF.UG.EDU.PL>
2023-10-31 15:25:39 +0100

    setUp acc added

--- app/tests/test_credits.py
+++ app/tests/test_credits.py
@@ -9,93 +9,95 @@ class TestCredits(unittest.TestCase):
     pesel = "12345678901"
     example_credit = 1000
 
+    def setUp(self):
+        self.acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
+
     def test_2_transfers(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        acc_personal.history = [-100, 100]
-        decision = acc_personal.take_credit(self.example_credit)
+        self.acc_personal.history = [-100, 100]
+        decision = self.acc_personal.take_credit(self.example_credit)
         self.assertEqual(
-            acc_personal.saldo,
+            self.acc_personal.saldo,
             0,
             "Kredyt  został udzielony, a nie powinien!",
         )
         self.assertFalse(decision, "Decyzja powinna być negatywna!")
 
     def test_3_incoming_transfers(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        acc_personal.history = [-100, 100, 100, 100]
-        decision = acc_personal.take_credit(self.example_credit)
+        self.acc_personal.history = [-100, 100, 100, 100]
+        decision = self.acc_personal.take_credit(self.example_credit)
         self.assertEqual(
-            acc_personal.saldo,
+            self.acc_personal.saldo,
             self.example_credit,
             "Kredyt nie został udzielony, a powinien!",
         )
         self.assertTrue(decision, "Decyzja powinna być pozytywna!")
 
     def test_3_incoming_transfers(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        acc_personal.history = [-100, 100, 100, 100]
-        decision = acc_personal.take_credit(self.example_credit)
+        self.acc_personal.history = [-100, 100, 100, 100]
+        decision = self.acc_personal.take_credit(self.example_credit)
         self.assertEqual(
-            acc_personal.saldo,
+            self.acc_personal.saldo,
             self.example_credit,
             "Kredyt nie został udzielony, a powinien!",
         )
         self.assertTrue(decision, "Decyzja powinna być pozytywna!")
 
     def test_mixed_transfers(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        acc_personal.history = [-100, 100, -100, 100]
-        decision = acc_personal.take_credit(self.example_credit)
-        self.assertEqual(acc_personal.saldo, 0, "Kredyt  został udzielony, a powinien!")
+        self.acc_personal.history = [-100, 100, -100, 100]
+        decision = self.acc_personal.take_credit(self.example_credit)
+        self.assertEqual(
+            self.acc_personal.saldo, 0, "Kredyt  został udzielony, a powinien!"
+        )
         self.assertFalse(decision, "Decyzja powinna być negatywna!")
 
     def test_five_last_positive_transactions(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        acc_personal.history = [-500, -500, 1000, self.example_credit, 1]  # sum = 1001
+        self.acc_personal.history = [
+            -500,
+            -500,
+            1000,
+            self.example_credit,
+            1,
+        ]  # sum = 1001
 
-        decision = acc_personal.take_credit(self.example_credit)
+        decision = self.acc_personal.take_credit(self.example_credit)
         self.assertEqual(
-            acc_personal.saldo,
+            self.acc_personal.saldo,
             self.example_credit,
             "Kredyt nie został udzielony, a powinien!",
         )
         self.assertTrue(decision, "Decyzja powinna być pozytywna!")
 
     def test_five_last_negative_transactions(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        acc_personal.history = [-500, -500, -500, -500, -500]  # sum = 1
+        self.acc_personal.history = [-500, -500, -500, -500, -500]  # sum = 1
 
-        decision = acc_personal.take_credit(self.example_credit)
+        decision = self.acc_personal.take_credit(self.example_credit)
         self.assertEqual(
-            acc_personal.saldo,
+            self.acc_personal.saldo,
             0,
             "Kredyt  został udzielony, a nie powinien!",
         )
         self.assertFalse(decision, "Decyzja powinna być negatywna!")
 
     def test_no_history_credit(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        decision = acc_personal.take_credit(self.example_credit)
-        acc_personal.history = []
+        decision = self.acc_personal.take_credit(self.example_credit)
+        self.acc_personal.history = []
         self.assertEqual(
-            acc_personal.saldo, 0, "Kredyt  został udzielony, a nie powinien!"
+            self.acc_personal.saldo, 0, "Kredyt  został udzielony, a nie powinien!"
         )
         self.assertFalse(decision, "Decyzja powinna być negatywna!")
 
     def test_negative_credit_amount(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        decision = acc_personal.take_credit(-1)
-        acc_personal.history = []
+        decision = self.acc_personal.take_credit(-1)
+        self.acc_personal.history = []
         self.assertEqual(
-            acc_personal.saldo, 0, "Kredyt  został udzielony, a nie powinien!"
+            self.acc_personal.saldo, 0, "Kredyt  został udzielony, a nie powinien!"
         )
         self.assertFalse(decision, "Decyzja powinna być negatywna!")
 
     def test_zero_credit_amount(self):
-        acc_personal = AccountPersonal(self.name, self.last_name, self.pesel)
-        decision = acc_personal.take_credit(0)
-        acc_personal.history = []
+        decision = self.acc_personal.take_credit(0)
+        self.acc_personal.history = []
         self.assertEqual(
-            acc_personal.saldo, 0, "Kredyt  został udzielony, a nie powinien!"
+            self.acc_personal.saldo, 0, "Kredyt  został udzielony, a nie powinien!"
         )
         self.assertFalse(decision, "Decyzja powinna być negatywna!")
