87204a368423c406739073ac8098ddb1e8819e63
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2023-11-11 20:48:50 +0100

    modification

--- app/tests/test_account_records.py
+++ app/tests/test_account_records.py
@@ -4,29 +4,29 @@ from ..AccountsRecord import AccountsRecord
 from ..Account_personal import AccountPersonal
 
 
-@classmethod
 class TestAccountRecords(unittest.TestCase):
     name = "Jan"
     last_name = "Kowalski"
     pesel = "12345678901"
     acc = AccountPersonal(name, last_name, pesel)
 
-
     @classmethod
     def SetUpClass(cls):
-        account = AccountPersonal(cls.name, cls.last_name, cls.pesel)
+        cls.account = AccountPersonal(cls.name, cls.last_name, cls.pesel)
 
-    def test_adding_accounts(self):
-        ...
+    def test_adding_accounts(cls, self):
+        AccountsRecord.add_acc_to_record(cls)
+        self.assertEqual(AccountsRecord.accounts, [self.acc, self.acc])
 
-    def test_searching_accounts(self, accounts_to_add, expected_register):
-        ...
+    def test_searching_accounts(self):
+        self.assertEqual(AccountsRecord.search_for_acc(self.pesel), self.acc)
 
-    def test_deleting_accounts(self, accounts_to_add, expected_register):
-        ...
+    def test_deleting_accounts(self):
+        AccountsRecord.delete_acc(self.pesel)
+        self.assertEqual(AccountsRecord.accounts, [])
 
-    def test_counting_accounts(self, accounts_to_add, expected_register):
-        ...
+    def test_counting_accounts(self):
+        self.assertEqual(AccountsRecord.number_of_acc(), 1)
 
     @classmethod
     def tearDownClass(cls):
