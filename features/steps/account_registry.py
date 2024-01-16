2a2245debe6499e6ad1ad15802c71b21b16b1f5a
Mateusz Kurowski <m.kurowski.023@studms.ug.edu.pl>
2024-01-16 23:24:19 +0100

    transfers bdd done

--- features/steps/account_registry.py
+++ features/steps/account_registry.py
@@ -1,11 +1,28 @@
-from behave import *
+from behave import step, when, then
 from selenium.webdriver.common.keys import Keys
 import requests
 from unittest_assertions import AssertEqual
 
-
 assert_equal = AssertEqual()
 URL = "http://localhost:5000"
+
+
+@step('Number of accounts in registry equals: "{count}"')
+def count_accs(context, count):
+    counted_accs = requests.get(URL + f"/api/accounts/count").json()["count"]
+    assert_equal(counted_accs, int(count))
+
+
+@step('Account with pesel "{pesel}" exists in registry')
+def account_with_pesel_exists(context, pesel):
+    assert_equal(requests.get(URL + f"/api/accounts/{pesel}").status_code, 200)
+
+
+@step('Account with pesel "{pesel}" does not exist in registry')
+def check_if_acc_with_pesel_does_not_exist(context, pesel):
+    new_pesel = pesel.strip('"')
+    resp = requests.get(URL + f"/api/accounts/{new_pesel}")
+    assert_equal(resp.status_code, 404)
 
 
 @when("I create an account using name: {name}, last name: {last_name}, pesel: {pesel}")
@@ -22,28 +39,10 @@ def create_acc(context, name, last_name, pesel):
     assert_equal(create_resp.status_code, 201)
 
 
-@step('Number of accounts in registry equals: "{count}"')
-def count_accs(context, count):
-    counted_accs = requests.get(URL + f"/api/accounts/count").json()["count"]
-    assert_equal(counted_accs, int(count))
-
-
-@step('Account with pesel "{pesel}" exists in registry')
-def account_with_pesel_exists(context, pesel):
-    assert_equal(requests.get(URL + f"/api/accounts/{pesel}").status_code, 200)
-
-
 @when('I delete account with pesel: "{pesel}"')
 def delete_acc(context, pesel):
     resp = requests.delete(URL + f"/api/accounts/{pesel}")
     assert_equal(resp.status_code, 200)
-
-
-@step('Account with pesel "{pesel}" does not exist in registry')
-def check_if_acc_with_pesel_does_not_exist(context, pesel):
-    new_pesel = pesel.strip('"')
-    resp = requests.get(URL + f"/api/accounts/{new_pesel}")
-    assert_equal(resp.status_code, 404)
 
 
 @when("I save the account registry")
@@ -69,6 +68,6 @@ def update_last_name(context, pesel, last_name):
 
 @then('Last name in account with pesel "{pesel}" is "{last_name}"')
 def check_last_name(context, pesel, last_name):
-    pesel_stripped = pesel.strip('"')
+    pesel_stripped = int(pesel.strip('"'))
     res = requests.get(URL + f"/api/accounts/{pesel_stripped}")
     assert_equal(res.json()["last_name"], last_name)
